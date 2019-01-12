9fea8979c32fe3039e5f2a70acc451ee89e44938
wuxin <1306539431@qq.com>
2019-01-13 01:18:04 +0800

    数据库迁移，静态文件集成

--- manager.py
+++ manager.py
@@ -1,8 +1,17 @@
-from info import create_app
+from info import create_app,db,models
+from flask_script import Manager
+from flask_migrate import MigrateCommand,Migrate
 
 app = create_app("develop")
 
+# 创建ｍａｎａｇｅｒ对象，管理ａｐｐ
+manager = Manager(app)
 
+# 使用Ｍｉｇｒａｔｅ关联ａｐｐ，ｄｂ
+Migrate(app,db)
+
+# 给ｍａｎａｇｅｒ添加一条操作条件
+manager.add_command("db",MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()

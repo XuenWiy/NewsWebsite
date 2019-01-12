deef2736da1d0d865c97e22adaf9da05eed999fa
wuxin <1306539431@qq.com>
2019-01-12 18:23:41 +0800

    创建配置信息文件，抽取初始化信息到ｉｎｆｏ的ｉｎｉｔ文件中

---
+++ config.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+from redis import StrictRedis
+
+# 设置配置信息
+class Config(object):
+    # 调试信息
+    DEBUG = True
+    SECRET_KEY = "fjewief"
+    # 数据库配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/secondnews"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # 配置ｓｅｓｓｉｏｎ信息
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=2)
+
+
+
+# 开发环境配置信息
+class DevelopConfig(Config):
+    pass
+
+
+
+# 生产环境配置信息
+class ProductConfig(Config):
+    DEBUG = False
+
+
+# 测试环境配置信息
+class TestConfig(Config):
+    pass
+
+# 提供一个统一的访问入口
+config_dict = {
+    "develop":DevelopConfig,
+    "product":ProductConfig,
+    "test":TestConfig
+
+}
--- manager.py
+++ manager.py
@@ -1,28 +1,13 @@
+from info import create_app
 
-
-
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-app = Flask(__name__)
-
-class Config(object):
-    # 调试信息
-    DEBUG = True
-
-    # 数据库配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/secondnews"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
-app.config.from_object(Config)
-
-#创建ＳＱＬＡｌｃｈｅｍｙ对象，关联ａｐｐ
-db = SQLAlchemy(app)
+app = create_app("develop")
 
 @app.route('/')
 def hello_world():
+    #
+    # redis_store.set("name","los")
+    # print(redis_store.get("name"))
+
     return 'Hello World!'
 
 

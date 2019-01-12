946ca18bdf62db54a4893e62d61dafc47c666fa0
wuxin <1306539431@qq.com>
2019-01-12 19:01:10 +0800

    １.视图函数中用到ｒｅｄｉｓ，但和蓝图加载到ａｐｐ中的代码会产生循环导包，所以可以将蓝图加载到ａｐｐ时在加载代码上导入蓝图 ２.ｒｅｄｉｓ对象在create_app中，所以必须设置成全局变量，否在其他地方不可以导入

--- manager.py
+++ manager.py
@@ -2,13 +2,6 @@ from info import create_app
 
 app = create_app("develop")
 
-@app.route('/')
-def hello_world():
-    #
-    # redis_store.set("name","los")
-    # print(redis_store.get("name"))
-
-    return 'Hello World!'
 
 
 if __name__ == '__main__':
